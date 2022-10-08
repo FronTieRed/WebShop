02bb4579165f2b1dc1b0c42d6f3b089e335d2049
FronTieRed <frontieredofficial@gmail.com>
2022-10-08 16:21:20 +0200

    Day 1 - Created default files and Basket file test + CMake

---
+++ src/backend/Basket/basket.cpp
@@ -0,0 +1,10 @@
+//
+// Created by front on 08/10/2022.
+//
+
+#include <iostream>
+#include "basket.h"
+
+int main() {
+    std::cout << "Basket is running.";
+}
---
+++ src/backend/Basket/basket.h
@@ -0,0 +1,14 @@
+//
+// Created by front on 08/10/2022.
+//
+
+#ifndef WEBSHOP_BASKET_H
+#define WEBSHOP_BASKET_H
+
+
+class basket {
+
+};
+
+
+#endif //WEBSHOP_BASKET_H
